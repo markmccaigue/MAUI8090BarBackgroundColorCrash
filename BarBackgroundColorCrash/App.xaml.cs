7b460b8e98dff401ede325cfe9032003c9577dc3
Mark McCaigue <mark.mccaigue@thrive.app>
2024-10-10 17:19:52 +0100

    Initial commit

---
+++ BarBackgroundColorCrash/App.xaml.cs
@@ -0,0 +1,23 @@
+﻿namespace BarBackgroundColorCrash;
+
+public class ContainerPage : FlyoutPage
+{
+    public ContainerPage()
+    {
+        Detail = new NavigationPage(new MainPage());
+        Flyout = new ContentPage
+        {
+            Title = "Flyout"
+        };
+    }
+}
+
+public partial class App : Application
+{
+    public App()
+    {
+        InitializeComponent();
+
+        MainPage = new ContainerPage();
+    }
+}
---
+++ BarBackgroundColorCrash/MainPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿namespace BarBackgroundColorCrash;
+
+public partial class MainPage : ContentPage
+{
+    private Random _random;
+    
+    public MainPage()
+    {
+        InitializeComponent();
+        
+        _random = new Random();
+    }
+
+    private void UpdatePrimaryColor()
+    {
+        App.Current.Resources["Primary"] = $"#{_random.Next(0x1000000):X6}";
+    }
+    
+    protected override void OnAppearing()
+    {
+        base.OnAppearing();
+
+        //UpdatePrimaryColor();
+    }
+
+    private void OnButtonClicked(object sender, EventArgs e)
+    {
+        UpdatePrimaryColor();
+    }
+}
